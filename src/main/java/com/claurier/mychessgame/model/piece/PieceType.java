4dcb153a200489c148f59ac654776b1b717066e4
wowwowster <wowwowster@gmail.com>
2023-03-15 17:27:21 +0100

    départ kelio

---
+++ src/main/java/com/claurier/mychessgame/model/piece/PieceType.java
@@ -0,0 +1,38 @@
+package jhaturanga.model.piece;
+
+/**
+ * The types of piece.
+ */
+public enum PieceType {
+
+    /**
+     * 
+     */
+    PAWN,
+
+    /**
+     * 
+     */
+    ROOK,
+
+    /**
+     * 
+     */
+    KNIGHT,
+
+    /**
+     * 
+     */
+    BISHOP,
+
+    /**
+     * 
+     */
+    QUEEN,
+
+    /**
+     * 
+     */
+    KING;
+
+}
